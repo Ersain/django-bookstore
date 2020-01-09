54e65a8a0c96d6686e4a63ad6a2d72cf7062a971
ydynmukhamed <dynmukhamed.ye@chocofood.kz>
2020-01-09 14:36:42 +0600

    Advanced User Registration

---
+++ pages/tests.py
@@ -0,0 +1,64 @@
+from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from pages.views import HomePageView, AboutPageView
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(
+            response, 'Hi there! I should not be here :D'
+        )
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response,
+            'Hi There! I should not be here!'
+        )
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
